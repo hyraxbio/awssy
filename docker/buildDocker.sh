ead66e4ba64ae321b3fea177a449e5b5e9ff401e
Andre Van Der Merwe <andre@andrevdm.com>
2019-04-08 19:07:16 +0200

    Use correct tag

--- docker/buildDocker.sh
+++ docker/buildDocker.sh
@@ -3,5 +3,5 @@ rm -rf temp
 mkdir -p temp/code
 rsync -av ../ temp/code/ --exclude='docker' --exclude='.stack-work' --exclude='.git' 
 docker build --tag=awssy$1 .
-docker tag awssy localhost:5987/awssy$1
+docker tag awssy$1 localhost:5987/awssy$1
 docker push localhost:5987/awssy$1
