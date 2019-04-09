a172994f56d0c9a5a9f743ae2a7dc38bad3d26b0
Andre Van Der Merwe <andre@andrevdm.com>
2019-04-09 13:06:35 +0200

    Use hyrax common haskell base layer

--- docker/buildDocker.sh
+++ docker/buildDocker.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+set -e
+
 rm -rf temp
 mkdir -p temp/code
+
+git clone git@github.com:hyraxbio/hyraxDocker.git temp/hyraxDocker
+cd temp/hyraxDocker
+./Dockerfile.haskell_13_12.sh
+
+cd ../..
 rsync -av ../ temp/code/ --exclude='docker' --exclude='.stack-work' --exclude='.git' 
-docker build --target build -t awssy__build .
 docker build --tag=awssy$1 .
 docker tag awssy$1 localhost:5987/awssy$1
 docker push localhost:5987/awssy$1
