3b28e6b93cde31591d0bf3c146a4fb1b5eaac7fc
Andre Van Der Merwe <andre@andrevdm.com>
2020-04-08 13:31:14 +0200

    docker update

--- docker/buildDocker.sh
+++ docker/buildDocker.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 set -e
 
-rm -rf temp
+\rm -rf temp
 mkdir -p temp/code
 
-git clone git@github.com:hyraxbio/hyraxDocker.git temp/hyraxDocker
-cd temp/hyraxDocker
-./Dockerfile.haskell_13_23.sh
-
-cd ../..
 rsync -av ../ temp/code/ --exclude='docker' --exclude='.stack-work' --exclude='.git' 
 docker build --tag=awssy$1 .
 docker tag awssy$1 localhost:5987/awssy$1
