24933fb8c73448dd72023aa8d8754576c5042e58
Andre Van Der Merwe <andre@andrevdm.com>
2020-04-09 15:12:04 +0200

    Fixed for using --mosh

--- docker/runAwssy.sh
+++ docker/runAwssy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp -r /root/_ssh /root/.ssh
 chmod 600 /root/.ssh
-/usr/bin/awssy --region Ireland --key /root/.ssh/hyraxbio.pem
+/usr/bin/awssy --region Ireland --key /root/.ssh/hyraxbio.pem "$@"
