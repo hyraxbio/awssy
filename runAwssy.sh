b086acd881b3dbfe227da098167433354db6eeaf
Andre Van Der Merwe <andre@andrevdm.com>
2021-08-16 15:56:13 +0200

    Remove region flag

--- runAwssy.sh
+++ runAwssy.sh
@@ -2,4 +2,4 @@
 cp -r /root/_ssh /root/.ssh
 chmod 600 /root/.ssh
 eval `ssh-agent`
-/usr/bin/awssy --region Ireland --key /root/.ssh/hyraxbio.pem "$@"
+/usr/bin/awssy --key /root/.ssh/hyraxbio.pem "$@"
